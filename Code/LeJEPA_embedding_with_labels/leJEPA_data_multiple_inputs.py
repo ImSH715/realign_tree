fffae055064cef8532ea54d9a22c66279033199b
James Oyaide <jamesoyaide@gmail.com>
2026-03-17 17:06:38 +0000

    file path changes

--- Code/LeJEPA_embedding_with_labels/leJEPA_data_multiple_inputs.py
+++ Code/LeJEPA_embedding_with_labels/leJEPA_data_multiple_inputs.py
@@ -14,8 +14,8 @@ from shapely.geometry import box
 from torch.utils.data import TensorDataset, DataLoader 
 
 # To access with the dataset folder
-BASE_DIR = r"Z:\ai4eo\Shared\2025_Forge\OSINFOR_data\01. Ortomosaicos\2023"
-ANNOTATED_COR = r"Z:\ai4eo\Shared\2025_Turing_L\Project\Annotated tree centroids\trees_32718.shp"
+BASE_DIR = r"\shared\ai4eo\Shared\2025_Forge\OSINFOR_data\01. Ortomosaicos\2023"
+ANNOTATED_COR = r"\shared\ai4eo\Shared\2025_Turing_L\Project\Annotated tree centroids\trees_32718.shp"
 
 IMG_SIZE = 448
 PATCH_SIZE = 16
