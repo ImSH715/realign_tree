8fb4932b59f4ec0d0fe0f0047fe1d87c50f0b506
Seunghyun Im <sim6@sheffield.com>
2026-03-27 13:17:04 +0000

    fixed oom kill

--- Code/Slide_grid/testing/lejepa_run.sh
+++ Code/Slide_grid/testing/lejepa_run.sh
@@ -27,6 +27,6 @@ echo "Using Python from: $(which python)"
 python --version
 
 # --- 4. Execution ---
-python -u lejepa_oom_killed.py
+python -u lejepa_oom_fixed.py
 
 echo "Job finished at $(date)"
