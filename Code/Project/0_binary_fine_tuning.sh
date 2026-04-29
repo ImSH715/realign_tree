1fd9d07ea6b4a97362d14c1a27839ffbc233b7c9
Seunghyun Im <sim6@sheffield.com>
2026-04-29 17:14:09 +0100

    adding binary fine tuning

---
+++ Code/Project/0_binary_fine_tuning.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#SBATCH --job-name=cpu_binary_lejepa
+#SBATCH --mem=82G
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=90:00:00
+#SBATCH --output=logs/0_binary_lejepa_%j.out
+#SBATCH --error=logs/0_binary_lejepa_%j.err
+#SBATCH --mail-type=END,FAIL
+
+mkdir -p logs
+
+module load Anaconda3
+eval "$(conda shell.bash hook)"
+conda activate lejepa
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+echo "Using Python from: $(which python)"
+python --version
+echo "Job started at $(date)"
+echo "Running on node: $(hostname)"
+
+python train_supervised_encoder.py \
+  --init_ckpt "./outputs/phase1_lejepa/phase1_encoder_best.pth" \
+  --train_shp "./outputs/splits_gt_binary_shihuahuaco/valid_points_shihuahuaco_binary_train.shp" \
+  --val_shp "./outputs/splits_gt_binary_shihuahuaco/valid_points_shihuahuaco_binary_val.shp" \
+  --imagery_root "/mnt/parscratch/users/acb20si/2025_Forge/OSINFOR_data/01. Ortomosaicos/2023" \
+  --output_dir "./outputs/phase1_lejepa_supervised_binary_shihuahuaco" \
+  --label_field "BinaryTree" \
+  --folder_field "Folder" \
+  --file_field "File" \
+  --fx_field "fx" \
+  --fy_field "fy" \
+  --coord_mode auto \
+  --image_size 224 \
+  --patch_size_px 224 \
+  --batch_size 8 \
+  --epochs 30 \
+  --lr_encoder 1e-6 \
+  --lr_head 1e-4 \
+  --freeze_encoder_epochs 3 \
+  --patience 8 \
+  --debug_patches 64 \
+  --balanced_sampler \
+  --num_workers 0 \
+  --device cuda
+
+echo "Job finished at $(date)"
