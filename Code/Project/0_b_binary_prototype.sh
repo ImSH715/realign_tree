760e9d372ed663ab4bfa8ca70aef703327371851
ImSH715 <seunghyunim2@gmail.com>
2026-04-30 02:56:49 +0100

    binary

--- Code/Project/0_b_binary_prototype.sh
+++ Code/Project/0_b_binary_prototype.sh
@@ -24,20 +24,14 @@ echo "Job started at $(date)"
 echo "Running on node: $(hostname)"
 
 python build_prototypes.py \
-  --encoder_ckpt "./outputs/binary_shihuahuaco_classweights_check/phase1_encoder_best.pth" \
-  --train_shp "./outputs/splits_binary/valid_points_train.shp" \
-  --imagery_root "/mnt/parscratch/users/acb20si/2025_Forge/OSINFOR_data/01. Ortomosaicos/2023" \
-  --output_dir "./outputs/phase2_binary_lejepa" \
-  --label_field Tree \
-  --folder_field Folder \
-  --file_field File \
-  --fx_field fx \
-  --fy_field fy \
-  --coord_mode auto \
-  --image_size 224 \
-  --patch_size_px 224 \
-  --batch_size 16 \
-  --num_workers 0 \
-  --device cpu
+  --phase1_ckpt "./outputs/binary_shihuahuaco_classweights_check/phase1_encoder_best.pth" \
+  --phase1_embedding_csv "./outputs/binary_shihuahuaco_classweights_check/phase1_embeddings.csv" \
+  --gt_path "./outputs/splits_binary/valid_points_train.shp" \
+  --gt_label_field BinaryTree \
+  --gt_folder_field Folder \
+  --gt_file_field File \
+  --gt_fx_field fx \
+  --gt_fy_field fy \
+  --output_dir "./outputs/phase2_binary_shihuahuaco"
 
 echo "Job finished at $(date)"
