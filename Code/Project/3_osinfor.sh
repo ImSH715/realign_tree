142128136b0fcc199a236d9a62b72b9ba458316a
Seunghyun Im <sim6@sheffield.com>
2026-05-06 23:16:48 +0100

    osinfor running

---
+++ Code/Project/3_osinfor.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# --- 1. Slurm Resource Configuration ---
+#SBATCH --job-name=osinfor
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=82G
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=99:00:00
+#SBATCH --output=logs/osinfor/phase3_new_censo_%j.out
+#SBATCH --error=logs/osinfor/phase3_new_censo_%j.err
+
+# --- 2. Email Notification Settings ---
+#SBATCH --mail-type=END,FAIL
+
+# --- 3. Environment Setup ---
+module load Anaconda3
+eval "$(conda shell.bash hook)"
+conda activate lejepa
+
+echo "Using Python from: $(which python)"
+python --version
+echo "Job started at $(date)"
+
+python run_pipeline.py \
+  --encoder_ckpt "./outputs/phase1_5_lejepa_cpu_binary_preprocess/phase1_encoder_best.pth" \
+  --prototypes_csv "./outputs/phase2_binary_shihuahuaco/class_prototypes_named.csv" \
+  --points_csv "Project/data/Censo_Forestal_shihuahuaco_overlap_only.csv" \
+  --imagery_root "/mnt/parscratch/users/acb20si/2025_Turing_L/datasets/Osinfor/Ortomosaicos" \
+  --output_csv "./outputs/phase3/censo_shihuahuaco_refined_gpu.csv" \
+  --tile_column "matched_tif" \
+  --point_id_column "point_id" \
+  --x_column "original_east" \
+  --y_column "original_north" \
+  --target_label_column "label" \
+  --coord_type world \
+  --search_radius_px 560 \
+  --coarse_step_px 32 \
+  --refine_radius_px 96 \
+  --refine_step_px 8 \
+  --similarity cosine \
+  --alpha 1.0 \
+  --beta 0.0002 \
+  --batch_size 32 \
+  --device cuda
+
+echo "Job finished at $(date)"
